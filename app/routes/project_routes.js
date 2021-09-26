4151b62d5cdb832bc07d24c82e81d6ddbe35ec92
zacharykogan <86493264+zacharykogan@users.noreply.github.com>
2021-09-26 02:49:23 -0400

    myprojects edit route

--- app/routes/project_routes.js
+++ app/routes/project_routes.js
@@ -102,7 +102,7 @@ router.post('/projects', requireToken, (req, res, next) => {
 
 // UPDATE
 // PATCH /projects/5a7db6c74d55bc51bdf39793
-router.patch('/projects/:id', requireToken, removeBlanks, (req, res, next) => {
+router.patch('/my-projects/:id', requireToken, removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
   delete req.body.project.owner
